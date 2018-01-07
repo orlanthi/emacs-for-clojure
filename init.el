e0bc3a682df8779ea37605ead6249c337948da5f
Brendan Boesen <orlanthi@gmail.com>
2018-01-07 16:16:32 +1100

    Upgraded cider according to README.md. Also upgraded packages in emacs known to be out of date. Modified editing.el and setup-clojure.el to handle changes in rainbow-mode and cider-mode respectively.

--- init.el
+++ init.el
@@ -10,6 +10,10 @@
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
 
 ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 ;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
@@ -138,3 +142,6 @@
 ;; Langauage-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
+
+(load "custom.el")
+
